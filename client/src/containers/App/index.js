5169d94fc1608c17b037e53b1a7203174f5c6954
James Geib <jpgeib2@gmail.com>
2020-07-28 12:09:02 -0400

    created exact paths for Home, Contact, and Portfolio pages in App

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../../pages/Home";
+import Contact from "../../pages/Contact";
+import Portfolio from "../../pages/Portfolio";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+
+class App extends Component {
+  
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
+      </Router>
+    );
+  }
 }
 
 export default App;
