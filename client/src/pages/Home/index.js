4eb4d14890d906f6dbc6c99fbe0b0872239fa7fa
James Geib <jpgeib2@gmail.com>
2020-07-30 11:02:53 -0400

    added preliminary styling to Home page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -6,28 +6,28 @@ import "./style.css";
 class Home extends Component {
     render() {
         return (
-            <Grid container>
+            <Grid container id="home-container">
                 <Grid.Row>
                     <Grid.Column width={4}>
-                        <Image src={ProfilePic} />
+                        <Image src={ProfilePic} size="massive" />
                     </Grid.Column>
                     <Grid.Column width={12}>
                         <div id="paragraphs">
-                            <div className="content1">
+                            <div id="content1">
                                 <p>
                                     I am a graduate of Fordham University with two Bachelor's
                                     degrees in Communications: Digital Technology and Emerging Media,
                                     and Film/Television Studies. Currently living in Oakland, CA.</p>
                             </div>
 
-                            <div className="content2">
+                            <div id="content2">
                                 <p>
                                     I am proficient in multiple Adobe Creative Cloud applications,
                                     have many years of experience working in Microsoft Office, and a basic knowledge of
                                     HTML5, CSS, Javascript, and SQL programming languages.</p>
                             </div>
 
-                            <div className="content3">
+                            <div id="content3">
                                 <p>
                                     I am ideally looking for a position that will allow me
                                     to utilize my pre-exisiting skills in Adobe, and improve
