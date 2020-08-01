23b9fbf0449c9725e1b570ac357e09ebdffaec74
James Geib <jpgeib2@gmail.com>
2020-08-01 13:56:21 -0400

    assigned the background image to div tag in Home page from root div in index.html

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,46 +7,48 @@ class Home extends Component {
     render() {
         return (
             <>
-            <Grid container>
-                <Grid.Row>
-                    <Header as="h1">James Geib</Header>
-                </Grid.Row>
-            </Grid>
-            <Grid container id="home-container">
-                <Grid.Row>
-                    <Grid.Column width={4}>
-                        <Image src={ProfilePic} size="massive" />
-                    </Grid.Column>
-                    <Grid.Column width={12}>
-                        <div id="paragraphs">
-                            <div id="content1">
-                                <p>
-                                    I am a graduate of Fordham University with two Bachelor's
-                                    degrees in Communications: Digital Technology and Emerging Media,
+                <div id="home-page">
+                    <Grid container>
+                        <Grid.Row>
+                            <Header as="h1">James Geib</Header>
+                        </Grid.Row>
+                    </Grid>
+                    <Grid container id="home-container">
+                        <Grid.Row>
+                            <Grid.Column width={4}>
+                                <Image src={ProfilePic} size="massive" />
+                            </Grid.Column>
+                            <Grid.Column width={12}>
+                                <div id="paragraphs">
+                                    <div id="content1">
+                                        <p>
+                                            I am a graduate of Fordham University with two Bachelor's
+                                            degrees in Communications: Digital Technology and Emerging Media,
                                     and Film/Television Studies. Currently living in Oakland, CA.</p>
-                            </div>
+                                    </div>
 
-                            <div id="content2">
-                                <p>
-                                    I am proficient in multiple Adobe Creative Cloud applications,
-                                    have many years of experience working in Microsoft Office, and a basic knowledge of
+                                    <div id="content2">
+                                        <p>
+                                            I am proficient in multiple Adobe Creative Cloud applications,
+                                            have many years of experience working in Microsoft Office, and a basic knowledge of
                                     HTML5, CSS, Javascript, and SQL programming languages.</p>
-                            </div>
+                                    </div>
 
-                            <div id="content3">
-                                <p>
-                                    I am ideally looking for a position that will allow me
-                                    to utilize my pre-exisiting skills in Adobe, and improve
-                                    upon them as I use them throughout the job. Alternatively, I am
-                                    also looking for a web development or data engineering position to
-                                    utilize my current set of coding languages. If you would like to see
+                                    <div id="content3">
+                                        <p>
+                                            I am ideally looking for a position that will allow me
+                                            to utilize my pre-exisiting skills in Adobe, and improve
+                                            upon them as I use them throughout the job. Alternatively, I am
+                                            also looking for a web development or data engineering position to
+                                            utilize my current set of coding languages. If you would like to see
                                     examples of my work, please consult my portfolio.</p>
-                            </div>
-                        </div>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </>
+                                    </div>
+                                </div>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            </>
         );
     }
 }
