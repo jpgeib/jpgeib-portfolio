924f08ea18d24c12442407cdcf5c00ea313917e8
James Geib <jpgeib2@gmail.com>
2020-08-12 11:51:06 -0400

    constructed preliminary home page and added links to github/linkedin, changed navbar background color to white

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
+import "./style.css";
+
 export default (props) => (
-    <Menu pointing secondary>
+    <Menu id="navbar" pointing secondary>
         <Menu.Item as={Link} content="Home" to="/" />
         <Menu.Item as={Link} content="Contact" to="/contact" />
         <Menu.Item as={Link} content="Portfolio" to="/portfolio" />
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Image, Header } from "semantic-ui-react";
+import { Grid, Image, Header, Icon } from "semantic-ui-react";
 import ProfilePic from "../../assets/images/james-profile.jpg";
 import "./style.css";
 
@@ -10,13 +10,26 @@ class Home extends Component {
                 <div id="home-page">
                     <Grid container>
                         <Grid.Row>
-                            <Header as="h1">James Geib</Header>
+                            <Header id="home-header" as="h1">James Geib</Header>
                         </Grid.Row>
                     </Grid>
                     <Grid container id="home-container">
                         <Grid.Row>
                             <Grid.Column width={4}>
-                                <Image src={ProfilePic} size="massive" />
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image id="profile-pic" src={ProfilePic} size="massive" />
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <div id="social-container">
+                                            <Header id="social-header" as="h2">Social Links:</Header>
+                                            <a href="https://github.com/jpgeib" target=":blank"><Icon id="github" size="huge" name="github" /></a>
+                                            <a href="https://www.linkedin.com/in/james-geib-0b6493160" target=":blank"><Icon id="linkedin" size="huge" name="linkedin" /></a>
+                                        </div>
+                                    </Grid.Column>
+                                </Grid.Row>
                             </Grid.Column>
                             <Grid.Column width={12}>
                                 <div id="paragraphs">
