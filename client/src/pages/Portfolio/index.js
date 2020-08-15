359052e65f15d92240f23994c98558d6f87e6e7c
James Geib <jpgeib2@gmail.com>
2020-08-15 16:20:00 -0400

    added header and subheader to Portfolio page, also added Adobe icons

--- client/src/pages/Portfolio/index.js
+++ client/src/pages/Portfolio/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import { Grid, Image, Header } from "semantic-ui-react";
+import PhotoShop from "../../assets/images/PhotoShop_Icon.png";
+import InDesign from "../../assets/images/InDesign-icon.png";
+import PremierePro from "../../assets/images/Premiere-Pro-icon.png";
+
+import "./style.css";
 
 class Portfolio extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Grid id="portfolio-container">
+                <Grid.Column width={16}>
+                    <Header as="h1">My Portfolio</Header>
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    <Header as="h3">Click on an icon/link to view my work:</Header>
+                </Grid.Column>
+                <Grid.Column>
+                    <Image size="small" src={PhotoShop} />
+                    <Image size="small" src={InDesign} />
+                    <Image size="small" src={PremierePro} />
+                </Grid.Column>
+            </Grid>
         );
     }
 }
