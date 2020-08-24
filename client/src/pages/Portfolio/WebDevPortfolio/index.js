57980f3403314e2a461cf1fdaf107a9a5806a3d3
James Geib <jpgeib2@gmail.com>
2020-08-23 21:09:32 -0400

    Created main header, subheader, and Grid structure for webdev portfolio page

--- client/src/pages/Portfolio/WebDevPortfolio/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/index.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
+import { Grid, Header, Image } from "semantic-ui-react";
+
+import "./style.css";
 
 class WebDevPortfolio extends Component {
     render() {
-        return(
-            <>
-                <h1>Web Developer</h1>
-            </>
+        return (
+            <div id="webdev-page">
+                <Grid container>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header id="webdev-header" as="h1">Full-Stack Web Development Portfolio</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column id="webdev-subheader" width={16}>
+                            <Header as="h3">Projects are listed in order of most recent with links provided:</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <Grid id="webdev-container" container>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
         );
     }
 };
