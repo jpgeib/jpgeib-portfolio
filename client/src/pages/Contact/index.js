9953e32d1126dcf1a63dd4ebe924cc468dcad2fb
James Geib <jpgeib2@gmail.com>
2020-09-01 20:50:07 -0400

    added a second section to Contact page with phone/email information

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -29,26 +29,42 @@ class Contact extends Component {
             });
     }
 
-    // handleInputChange = (e) => {
-    //     console.log(e.target);
-    // }
-
     render() {
         return (
-            <Grid id="contact-container">
-                <Grid.Column width={16}>
-                    <Header id="contact-header" as="h1">Contact</Header>
-                </Grid.Column>
-                <Grid.Column width={16}>
-                    <Header id="contact-subheader" as="h3">Please enter the following information:</Header>
-                </Grid.Column>
-                <Grid.Column width={16}>
-                    <ContactForm 
-                        onSubmit={this.onSubmit}
-                        success={this.state.success}
-                    />
-                </Grid.Column>
-            </Grid>
+            <div id="contact-page">
+                <Grid id="contact-header-container">
+                    <Grid.Column width={16}>
+                        <Header id="contact-header" as="h1">Contact</Header>
+                    </Grid.Column>
+                    <Grid.Column width={16}>
+                        <Header id="contact-subheader1" as="h3">You can reach me directly by phone or email:</Header>
+                    </Grid.Column>
+                </Grid>
+                <Grid id="contact-container1">
+                    <Grid.Column width={8}>
+                        <Header id="contact-phone" as="h2">Phone: +1 (203)-736-7540</Header>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Header id="contact-email" as="h2">E-mail: jpgeib2@gmail.com</Header>
+                    </Grid.Column>
+                </Grid>
+                <Grid>
+                    <Grid.Column width={16}>
+                        <Header id="OR-connector" as="h1">OR</Header>
+                    </Grid.Column>
+                </Grid>
+                <Grid id="contact-container2">
+                    <Grid.Column width={16}>
+                        <Header id="contact-subheader2" as="h3">Please enter the following information below:</Header>
+                    </Grid.Column>
+                    <Grid.Column width={16}>
+                        <ContactForm
+                            onSubmit={this.onSubmit}
+                            success={this.state.success}
+                        />
+                    </Grid.Column>
+                </Grid>
+            </div>
         );
     }
 }
