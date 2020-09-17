6418a8534c2bf4ac551c16a852b80a0970c501ed
James Geib <jpgeib2@gmail.com>
2020-09-16 20:12:04 -0400

    built Grid structure for headers container in SkillSection

--- client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
@@ -7,7 +7,26 @@ class SkillSection extends Component {
     render() {
         return (
             <div id="skills-page">
-                <Header as="h1">Skills</Header>
+                <Grid id="skills-headers-container">
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header id="skills-header" as="h1">Full-Stack Web Development Technical Skills</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Header id="skills-subheader" as="h3">I am proficient in the following technologies:</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <Grid>
+                    <Grid.Row>
+
+                    </Grid.Row>
+                    <Grid.Row>
+                        
+                    </Grid.Row>
+                </Grid>
             </div>
         );
     }
