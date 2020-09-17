556e45389b7f3b9637f952c3b690b0dd00a3eca6
James Geib <jpgeib2@gmail.com>
2020-09-16 20:24:25 -0400

    added icons and headers for HTML5, CSS, and JavaScript in SkillSection

--- client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Grid, Header, Image, List } from "semantic-ui-react";
+import HTML5 from "../../../../assets/images/HTML-icon.png";
+import CSS from "../../../../assets/images/CSS-icon.png";
+import JavaScript from "../../../../assets/images/Javascript-icon.png";
 
 import "./style.css";
 
@@ -19,12 +22,30 @@ class SkillSection extends Component {
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
-                <Grid>
+                <Grid id="skills-container">
                     <Grid.Row>
-
+                        <Grid.Column width={8}>
+                            <Image src={HTML5} size="small" />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Header id="html5-header" as="h2">HTML5</Header>
+                        </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
-                        
+                        <Grid.Column width={8}>
+                            <Image src={CSS} size="small" />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Header id="css-header" as="h2">CSS</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image src={JavaScript} size="small" />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Header id="javascript-header" as="h2">JavaScript</Header>
+                        </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </div>
