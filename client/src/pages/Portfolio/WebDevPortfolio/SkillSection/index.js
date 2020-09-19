b86c9f2d6c9a0809809b0fb46414849227790a79
James Geib <jpgeib2@gmail.com>
2020-09-19 13:17:50 -0400

    reformatted technologies in SkillSection

--- client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
@@ -30,66 +30,106 @@ class SkillSection extends Component {
                 <Grid id="skills-container">
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Image src={HTML5} size="small" />
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={HTML5} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="html5-header" as="h2">HTML5</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Header id="html5-header" as="h2">HTML5</Header>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image src={CSS} size="small" />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Header id="css-header" as="h2">CSS</Header>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image src={JavaScript} size="small" />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Header id="javascript-header" as="h2">JavaScript</Header>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image src={jQuery} size="small" />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Header id="jquery-header" as="h2">jQuery</Header>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={CSS} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="css-header" as="h2">CSS</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Image src={SQL} size="small" />
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={JavaScript} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="javascript-header" as="h2">JavaScript</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Header id="sql-header" as="h2">SQL</Header>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={jQuery} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="jquery-header" as="h2">jQuery</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Image src={MongoDB} size="small" />
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={SQL} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="sql-header" as="h2">SQL</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Header id="mongo-header" as="h2">MongoDB</Header>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image id="mongo-logo" src={MongoDB} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="mongo-header" as="h2">MongoDB</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Image src={ReactLogo} size="small" />
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={ReactLogo} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="react-header" as="h2">React.js</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Header id="react-header" as="h2">React.js</Header>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image src={ReduxLogo} size="small" />
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Header id="redux-header" as="h2">Redux.js</Header>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image id="redux-logo" src={ReduxLogo} size="small" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header id="redux-header" as="h2">Redux.js</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
