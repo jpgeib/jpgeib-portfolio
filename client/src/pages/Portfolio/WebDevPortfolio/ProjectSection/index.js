ce6b0738af430224ef59973b0de2d8592497de87
James Geib <jpgeib2@gmail.com>
2020-10-01 13:58:03 -0400

    rendered each screen size subfolder in main index.js of ProjectSection

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import ProjectSectionMobile from "./ProjectSectionMobile";
+import ProjectSectionTablet from "./ProjectSectionTablet";
 import ProjectSectionComputer from "./ProjectSectionComputer";
+import ProjectSectionLarge from "./ProjectSectionLarge";
 import ProjectSectionWide from "./ProjectSectionWide";
 
 import "./style.css";
@@ -8,7 +11,10 @@ class ProjectSection extends Component {
     render() {
         return (
             <>
+                <ProjectSectionMobile />
+                <ProjectSectionTablet />
                 <ProjectSectionComputer />
+                <ProjectSectionLarge />
                 <ProjectSectionWide />
             </>
             // <div id="project-page">
