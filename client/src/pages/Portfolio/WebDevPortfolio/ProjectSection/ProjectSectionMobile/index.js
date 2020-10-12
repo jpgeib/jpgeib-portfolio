caa5346505fdc36ed602f1c52b165cabd5d850a3
James Geib <jpgeib2@gmail.com>
2020-10-12 16:33:39 -0400

    created entry for AllChat V1 in ProjectSectionMobile

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionMobile/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionMobile/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Header, Image, List, Responsive, Icon } from "semantic-ui-react";
-import AllChat from "../../../../../assets/images/AllChatV2.png";
+import AllChatV2 from "../../../../../assets/images/AllChatV2.png";
+import AllChatV1 from "../../../../../assets/images/AllChatV1.png";
 import FeastFitness from "../../../../../assets/images/Feast-Fitness.png";
 import OnlinePortfolio from "../../../../../assets/images/jpgeib-portfolio.png";
 import HerokuIcon from "../../../../../assets/images/heroku-icon.png";
@@ -61,22 +62,22 @@ class ProjectSectionMobile extends Component {
                     </Responsive>
                     <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Row}>
                         <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
-                            <Image id="allchat-image-mobile" src={AllChat} size="big" />
+                            <Image id="allchatv2-image-mobile" src={AllChatV2} size="big" />
                         </Responsive>
                         <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
-                            <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid} id="allchat-container-mobile">
+                            <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid} id="allchatv2-container-mobile">
                                 <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Row}>
                                     <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
-                                        <Header id="allchat-header-mobile" as="h2">AllChat</Header>
+                                        <Header id="allchatv2-header-mobile" as="h2">AllChat V2</Header>
                                     </Responsive>
                                     <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
-                                        <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid} id="allchat-links-container-mobile">
+                                        <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid} id="allchatv2-links-container-mobile">
                                             <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Row}>
                                                 <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={8}>
-                                                    <Header id="github-header-mobile" as="h3"><a id="allchat-github-mobile" href="https://github.com/paulmhan/AllChat-v2" target=":blank"><Icon id="github" size="huge" name="github" /></a></Header>
+                                                    <Header id="github-header-mobile" as="h3"><a id="allchatv2-github-mobile" href="https://github.com/paulmhan/AllChat-v2" target=":blank"><Icon id="github" size="huge" name="github" /></a></Header>
                                                 </Responsive>
                                                 <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={8}>
-                                                    <Header id="heroku-header-mobile" as="h3"><a id="allchat-heroku-mobile" href="https://all-chat-v2.herokuapp.com" target=":blank"><Image src={HerokuIcon} size="tiny" /></a></Header>
+                                                    <Header id="heroku-header-mobile" as="h3"><a id="allchatv2-heroku-mobile" href="https://all-chat-v2.herokuapp.com" target=":blank"><Image src={HerokuIcon} size="tiny" /></a></Header>
                                                 </Responsive>
                                             </Responsive>
                                         </Responsive>
@@ -87,7 +88,42 @@ class ProjectSectionMobile extends Component {
                                         <List bulleted size="big">
                                             <List.Item className="project-list-item-mobile">An online chatroom app capable of translating messages from one language to another, broadening the scope of communication.</List.Item>
                                             <List.Item className="project-list-item-mobile">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
-                                            <List.Item className="project-list-item-mobile">Technologies used: React/Redux, CSS, Adobe Photoshop</List.Item>
+                                            <List.Item className="project-list-item-mobile">Technologies used: React/Redux, Semantic UI React Framework, CSS, Adobe Photoshop</List.Item>
+                                        </List>
+                                    </Responsive>
+                                </Responsive>
+                            </Responsive>
+                        </Responsive>
+                    </Responsive>
+                    <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Row}>
+                        <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
+                            <Image id="allchatv1-image-mobile" src={AllChatV1} size="big" />
+                        </Responsive>
+                        <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
+                            <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid} id="allchatv1-container-mobile">
+                                <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Row}>
+                                    <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
+                                        <Header id="allchatv1-header-mobile" as="h2">AllChat V1</Header>
+                                    </Responsive>
+                                    <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
+                                        <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid} id="allchatv1-links-container-mobile">
+                                            <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Row}>
+                                                <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={8}>
+                                                    <Header id="github-header-mobile" as="h3"><a id="allchatv1-github-mobile" href="https://github.com/paulmhan/AllChat-v2" target=":blank"><Icon id="github" size="huge" name="github" /></a></Header>
+                                                </Responsive>
+                                                <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={8}>
+                                                    <Header id="heroku-header-mobile" as="h3"><a id="allchatv1-heroku-mobile" href="https://all-chat-v2.herokuapp.com" target=":blank"><Image src={HerokuIcon} size="tiny" /></a></Header>
+                                                </Responsive>
+                                            </Responsive>
+                                        </Responsive>
+                                    </Responsive>
+                                </Responsive>
+                                <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Row}>
+                                    <Responsive {...Responsive.onlyMobile} maxWidth={414} as={Grid.Column} width={16}>
+                                        <List bulleted size="big">
+                                            <List.Item className="project-list-item-mobile">A basic online chatroom app that serves as the foundational stepping stone for an even more ambitious project.</List.Item>
+                                            <List.Item className="project-list-item-mobile">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
+                                            <List.Item className="project-list-item-mobile">Technologies used: React, Semantic UI React Framework, CSS</List.Item>
                                         </List>
                                     </Responsive>
                                 </Responsive>
