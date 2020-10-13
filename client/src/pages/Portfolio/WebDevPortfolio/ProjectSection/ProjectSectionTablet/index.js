1afd6c319dd28233524f1862d65894f7b202f4f4
James Geib <jpgeib2@gmail.com>
2020-10-12 21:53:17 -0400

    added AllChat V1 entry to ProjectSectionTablet

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionTablet/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionTablet/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Header, Image, List, Responsive } from "semantic-ui-react";
-import AllChat from "../../../../../assets/images/AllChatV2.png";
+import AllChatV2 from "../../../../../assets/images/AllChatV2.png";
+import AllChatV1 from "../../../../../assets/images/AllChatV1.png";
 import FeastFitness from "../../../../../assets/images/Feast-Fitness.png";
 import OnlinePortfolio from "../../../../../assets/images/jpgeib-portfolio.png";
 
@@ -52,17 +53,17 @@ class ProjectSectionTablet extends Component {
                     </Responsive>
                     <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
                         <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
-                            <Image id="allchat-image-tablet" src={AllChat} size="big" />
+                            <Image id="allchatv2-image-tablet" src={AllChatV2} size="big" />
                         </Responsive>
                         <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
-                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="allchat-container-tablet">
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="allchatv2-container-tablet">
                                 <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
                                     <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
-                                        <Header id="allchat-header-tablet" as="h2">AllChat</Header>
+                                        <Header id="allchatv2-header-tablet" as="h2">AllChat V2</Header>
                                     </Responsive>
                                     <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
-                                        <Header id="github-header-tablet" as="h3">Github: <a id="allchat-github-tablet" href="https://github.com/paulmhan/AllChat-v2" target=":blank">https://github.com/paulmhan/AllChat-v2</a></Header>
-                                        <Header id="heroku-header-tablet" as="h3">Heroku: <a id="allchat-heroku-tablet" href="https://all-chat-v2.herokuapp.com" target=":blank">https://all-chat-v2.herokuapp.com</a></Header>
+                                        <Header id="github-header-tablet" as="h3">Github: <a id="allchatv2-github-tablet" href="https://github.com/paulmhan/AllChat-v2" target=":blank">https://github.com/paulmhan/AllChat-v2</a></Header>
+                                        <Header id="heroku-header-tablet" as="h3">Heroku: <a id="allchatv2-heroku-tablet" href="https://all-chat-v2.herokuapp.com" target=":blank">https://all-chat-v2.herokuapp.com</a></Header>
                                     </Responsive>
                                 </Responsive>
                                 <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
@@ -70,7 +71,34 @@ class ProjectSectionTablet extends Component {
                                         <List bulleted size="big">
                                             <List.Item className="project-list-item-tablet">An online chatroom app capable of translating messages from one language to another, broadening the scope of communication.</List.Item>
                                             <List.Item className="project-list-item-tablet">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
-                                            <List.Item className="project-list-item-tablet">Technologies used: React/Redux, CSS, Adobe Photoshop</List.Item>
+                                            <List.Item className="project-list-item-tablet">Technologies used: React/Redux, Semantic UI React Framework, CSS, Adobe Photoshop</List.Item>
+                                        </List>
+                                    </Responsive>
+                                </Responsive>
+                            </Responsive>
+                        </Responsive>
+                    </Responsive>
+                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
+                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                            <Image id="allchatv1-image-tablet" src={AllChatV1} size="big" />
+                        </Responsive>
+                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="allchatv1-container-tablet">
+                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
+                                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                                        <Header id="allchatv1-header-tablet" as="h2">AllChat V1</Header>
+                                    </Responsive>
+                                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                                        <Header id="github-header-tablet" as="h3">Github: <a id="allchatv1-github-tablet" href="https://github.com/paulmhan/AllChat" target=":blank">https://github.com/paulmhan/AllChat</a></Header>
+                                        <Header id="heroku-header-tablet" as="h3">Heroku: <a id="allchatv1-heroku-tablet" href="https://all-chat-app.herokuapp.com/" target=":blank">https://all-chat-app.herokuapp.com/</a></Header>
+                                    </Responsive>
+                                </Responsive>
+                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
+                                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                                        <List bulleted size="big">
+                                            <List.Item className="project-list-item-tablet">A basic online chatroom app that serves as the foundational stepping stone for an even more ambitious project.</List.Item>
+                                            <List.Item className="project-list-item-tablet">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
+                                            <List.Item className="project-list-item-tablet">Technologies used: React, Semantic UI React Framework, CSS</List.Item>
                                         </List>
                                     </Responsive>
                                 </Responsive>
