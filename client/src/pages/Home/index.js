2b612c501f33f2e1a8f4609f41652fedadf273b8
James Geib <jpgeib2@gmail.com>
2021-01-28 13:55:22 -0500

    imported homeText object to Home page and rendered text variables where needed

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Grid, Image, Header, Icon } from "semantic-ui-react";
 import ProfilePic from "../../assets/images/james-profile.jpg";
+import { homeText } from "../../text"; 
+
 import "./style.css";
 
 class Home extends Component {
@@ -21,7 +23,6 @@ class Home extends Component {
                                         <Image id="profile-pic" src={ProfilePic} size="massive" />
                                     </Grid.Column>
                                 </Grid.Row>
-
                                 <Grid.Row>
                                     <Grid.Column width={16}>
                                         <div id="contact-container">
@@ -44,34 +45,13 @@ class Home extends Component {
                             <Grid.Column width={12}>
                                 <div id="paragraphs">
                                     <div id="content1">
-                                        <p>
-                                            I am a graduate of UC Berkeley Extension's Coding Boot Camp with a 
-                                            certificate in Full-Stack Web Development, and Fordham University 
-                                            with two Bachelor's degrees in Communications: Digital Technology and 
-                                            Emerging Media, and Film/Television Studies. Currently living in Milford, 
-                                            CT; willing to relocate if necessary.
-                                        </p>
+                                        <p>{homeText.paragraph1}</p>
                                     </div>
                                     <div id="content2">
-                                        <p>
-                                            I am proficient in HTML5, CSS/CSS Frameworks, Javascript/jQuery, and SQL coding languages, and have
-                                            experience working in MERN stack development. I specialize mostly in front-end development and design,
-                                            enhanced by knowledge in Adobe Creative Cloud applications, but I can also work in back-end development
-                                            when needed.
-                                        </p>
+                                        <p>{homeText.paragraph2}</p>
                                     </div>
                                     <div id="content3">
-                                        <p>
-                                            I am ideally looking for a position that will allow me
-                                            to utilize my ability to create an eye-catching, yet simple 
-                                            user-friendly interface for websites, apps, and other digital 
-                                            media, in order to enhance the user experience with a visually 
-                                            optimized client-side service. Having been on the consumer side of 
-                                            various software, I understand the frustration users may have when navigating
-                                            through different pages/menus just to find one small piece of information. My mission
-                                            as a front-end developer is to create an intuitive, easy-to-follow format that minimizes
-                                            any confusion or difficulty in using.
-                                        </p>
+                                        <p>{homeText.paragraph3}</p>
                                     </div>
                                 </div>
                             </Grid.Column>
