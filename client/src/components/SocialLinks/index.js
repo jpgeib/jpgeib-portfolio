f8cae3a2d951b7c88b592e554c615c6b951f7a89
James Geib <jpgeib2@gmail.com>
2021-01-29 00:45:07 -0500

    created SocialLinks component to store social links structure and clean up code in Home page

---
+++ client/src/components/SocialLinks/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header, Icon } from "semantic-ui-react";
+
+const SocialLinks = () => {
+    return (
+        <div id="social-container">
+            <Header id="social-header" as="h2">Social Links:</Header>
+            <a href="https://github.com/jpgeib" target=":blank"><Icon id="github" size="huge" name="github" /></a>
+            <a href="https://www.linkedin.com/in/james-geib-0b6493160" target=":blank"><Icon id="linkedin" size="huge" name="linkedin" /></a>
+        </div>
+    );
+}
+
+export default SocialLinks;
