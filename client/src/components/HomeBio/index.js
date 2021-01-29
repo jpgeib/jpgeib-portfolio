fd77dce0ae897aa574f26533a3bc40effe7d7f65
James Geib <jpgeib2@gmail.com>
2021-01-29 00:36:41 -0500

    created a HomeBio component for the Home page paragraphs structure

---
+++ client/src/components/HomeBio/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { homeText } from "../../text";
+
+const HomeBio = () => {
+    return (
+        <div id="paragraphs">
+            <div id="content1">
+                <p>{homeText.paragraph1}</p>
+            </div>
+            <div id="content2">
+                <p>{homeText.paragraph2}</p>
+            </div>
+            <div id="content3">
+                <p>{homeText.paragraph3}</p>
+            </div>
+        </div>
+    );
+};
+
+export default HomeBio;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Image, Header, Icon } from "semantic-ui-react";
 import ProfilePic from "../../assets/images/james-profile.jpg";
-import { homeText } from "../../text"; 
+import HomeBio from "../../components/HomeBio";
 
 import "./style.css";
 
@@ -43,17 +43,7 @@ class Home extends Component {
                                 </Grid.Row>
                             </Grid.Column>
                             <Grid.Column width={12}>
-                                <div id="paragraphs">
-                                    <div id="content1">
-                                        <p>{homeText.paragraph1}</p>
-                                    </div>
-                                    <div id="content2">
-                                        <p>{homeText.paragraph2}</p>
-                                    </div>
-                                    <div id="content3">
-                                        <p>{homeText.paragraph3}</p>
-                                    </div>
-                                </div>
+                                <HomeBio />
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
