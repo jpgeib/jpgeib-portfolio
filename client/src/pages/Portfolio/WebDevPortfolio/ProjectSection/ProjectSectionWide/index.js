1df0c7dd74501f860747a902f67d4b12d92f3205
James Geib <jpgeib2@gmail.com>
2021-05-20 12:48:14 -0400

    applied same changes/relocation for widescreen view of portfolio entry

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionWide/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionWide/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Grid, Header, Image, List, Responsive } from "semantic-ui-react";
+import OnlinePortfolioWide from "../../../../../components/OnlinePortfolio/WideScreen";
 import AllChatV2 from "../../../../../assets/images/AllChatV2.png";
 import AllChatV1 from "../../../../../assets/images/AllChatV1.png";
-import OnlinePortfolio from "../../../../../assets/screenshots/jpgeib-portfolio.png";
 
 import "./style.css";
 
@@ -23,33 +23,7 @@ class ProjectSectionWide extends Component {
                     </Responsive>
                 </Responsive>
                 <Responsive {...Responsive.onlyWidescreen} as={Grid} id="project-container-wide">
-                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                        <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={8}>
-                            <Image src={OnlinePortfolio} size="big" />
-                        </Responsive>
-                        <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={8}>
-                            <Responsive {...Responsive.onlyWidescreen} as={Grid} id="portfolio-container-wide">
-                                <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={5}>
-                                        <Header id="portfolio-header-wide" as="h2">Online Portfolio</Header>
-                                    </Responsive>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={11}>
-                                        <Header id="github-header-wide" as="h3">Github: <a id="portfolio-github-wide" href="https://github.com/jpgeib/jpgeib-portfolio" target=":blank">https://github.com/jpgeib/jpgeib-portfolio</a></Header>
-                                        <Header id="heroku-header-wide" as="h3">Heroku: <a id="portfolio-heroku-wide" href="https://jpgeib-portfolio.herokuapp.com/" target=":blank">https://jpgeib-portfolio.herokuapp.com/</a></Header>
-                                    </Responsive>
-                                </Responsive>
-                                <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={16}>
-                                        <List bulleted size="big">
-                                            <List.Item className="project-list-item-wide">An online portfolio created to list and show off my achievements/works in full-stack web development and digital media production.</List.Item>
-                                            <List.Item className="project-list-item-wide">Role/Responsibility: Front-End Development, Front-End Design, Back-End Development</List.Item>
-                                            <List.Item className="project-list-item-wide">Technologies used: React/Redux, CSS, Semantic UI React Framework, Express.js, Adobe Photoshop</List.Item>
-                                        </List>
-                                    </Responsive>
-                                </Responsive>
-                            </Responsive>
-                        </Responsive>
-                    </Responsive>
+                    <OnlinePortfolioWide />
                     <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
                         <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={8}>
                             <Image src={AllChatV2} size="big" />
