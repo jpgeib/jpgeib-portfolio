42792dff2a727748a39c5c4aee3d2b8d99a93955
James Geib <jpgeib2@gmail.com>
2021-05-20 19:43:21 -0400

    applied same changes to the wide screen view

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionWide/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionWide/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Grid, Header, Image, List, Responsive } from "semantic-ui-react";
+import { Grid, Header, Responsive } from "semantic-ui-react";
 import OnlinePortfolioWide from "../../../../../components/OnlinePortfolio/WideScreen";
-import AllChatV2 from "../../../../../assets/screenshots/AllChatV2.png";
-import AllChatV1 from "../../../../../assets/screenshots/AllChatV1.png";
+import AllChatV2Wide from "../../../../../components/AllChatV2/WideScreen";
+import AllChatV1Wide from "../../../../../components/AllChat/WideScreen";
 
 import "./style.css";
 
@@ -24,60 +24,8 @@ class ProjectSectionWide extends Component {
                 </Responsive>
                 <Responsive {...Responsive.onlyWidescreen} as={Grid} id="project-container-wide">
                     <OnlinePortfolioWide />
-                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                        <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={8}>
-                            <Image src={AllChatV2} size="big" />
-                        </Responsive>
-                        <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={8}>
-                            <Responsive {...Responsive.onlyWidescreen} as={Grid} id="allchatv2-container-wide">
-                                <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={5}>
-                                        <Header id="allchatv2-header-wide" as="h2">AllChat V2</Header>
-                                    </Responsive>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={11}>
-                                        <Header id="github-header-wide" as="h3">Github: <a id="allchatv2-github-wide" href="https://github.com/paulmhan/AllChat-v2" target=":blank">https://github.com/paulmhan/AllChat-v2</a></Header>
-                                        <Header id="heroku-header-wide" as="h3">Heroku: <a id="allchatv2-heroku-wide" href="https://all-chat-v2.herokuapp.com" target=":blank">https://all-chat-v2.herokuapp.com</a></Header>
-                                    </Responsive>
-                                </Responsive>
-                                <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={16}>
-                                        <List bulleted size="big">
-                                            <List.Item className="project-list-item-wide">An online chatroom app capable of translating messages from one language to another, broadening the scope of communication.</List.Item>
-                                            <List.Item className="project-list-item-wide">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
-                                            <List.Item className="project-list-item-wide">Technologies used: React/Redux, Semantic UI React Framework, CSS, Adobe Photoshop</List.Item>
-                                        </List>
-                                    </Responsive>
-                                </Responsive>
-                            </Responsive>
-                        </Responsive>
-                    </Responsive>
-                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                        <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={8}>
-                            <Image src={AllChatV1} size="big" />
-                        </Responsive>
-                        <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={8}>
-                            <Responsive {...Responsive.onlyWidescreen} as={Grid} id="allchatv1-container-wide">
-                                <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={5}>
-                                        <Header id="allchatv1-header-wide" as="h2">AllChat V1</Header>
-                                    </Responsive>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={11}>
-                                        <Header id="github-header-wide" as="h3">Github: <a id="allchatv1-github-wide" href="https://github.com/paulmhan/AllChat" target=":blank">https://github.com/paulmhan/AllChat</a></Header>
-                                        <Header id="heroku-header-wide" as="h3">Heroku: <a id="allchatv1-heroku-wide" href="https://all-chat-app.herokuapp.com/" target=":blank">https://all-chat-app.herokuapp.com/</a></Header>
-                                    </Responsive>
-                                </Responsive>
-                                <Responsive {...Responsive.onlyWidescreen} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyWidescreen} as={Grid.Column} width={16}>
-                                        <List bulleted size="big">
-                                            <List.Item className="project-list-item-wide">A basic online chatroom app that serves as the foundational stepping stone for an even more ambitious project.</List.Item>
-                                            <List.Item className="project-list-item-wide">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
-                                            <List.Item className="project-list-item-wide">Technologies used: React, Semantic UI React Framework, CSS</List.Item>
-                                        </List>
-                                    </Responsive>
-                                </Responsive>
-                            </Responsive>
-                        </Responsive>
-                    </Responsive>
+                    <AllChatV2Wide />
+                    <AllChatV1Wide />
                 </Responsive>
             </div>
         );
