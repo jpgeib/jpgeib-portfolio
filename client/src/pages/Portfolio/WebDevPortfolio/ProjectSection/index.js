2676c71d2e04cdcfaaaf7c5450a61dc39a69dde2
James Geib <jpgeib2@gmail.com>
2021-05-20 11:20:27 -0400

    created initial folder structure for OnlinePortfolio component and its subcomponents

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
@@ -5,8 +5,6 @@ import ProjectSectionComputer from "./ProjectSectionComputer";
 import ProjectSectionLarge from "./ProjectSectionLarge";
 import ProjectSectionWide from "./ProjectSectionWide";
 
-import "./style.css";
-
 class ProjectSection extends Component {
     render() {
         return (
