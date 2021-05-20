20d99ac92a3d77c6972de558a1f2dddb61923e52
James Geib <jpgeib2@gmail.com>
2021-05-20 12:24:59 -0400

    moved online portfolio project entry into its own component for large screen view

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionLarge/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/ProjectSectionLarge/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Grid, Header, Image, List, Responsive } from "semantic-ui-react";
+import OnlinePortfolioLarge from "../../../../../components/OnlinePortfolio/LargeScreen";
 import AllChatV2 from "../../../../../assets/images/AllChatV2.png";
 import AllChatV1 from "../../../../../assets/images/AllChatV1.png";
-import OnlinePortfolio from "../../../../../assets/screenshots/jpgeib-portfolio.png";
 
 import "./style.css";
 
@@ -23,33 +23,7 @@ class ProjectSectionLarge extends Component {
                     </Responsive>
                 </Responsive>
                 <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid} id="project-container-large">
-                    <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Row}>
-                        <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Column} width={8}>
-                            <Image src={OnlinePortfolio} size="big" />
-                        </Responsive>
-                        <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Column} width={8}>
-                            <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid} id="portfolio-container-large">
-                                <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Column} width={5}>
-                                        <Header id="portfolio-header-large" as="h2">Online Portfolio</Header>
-                                    </Responsive>
-                                    <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Column} width={11}>
-                                        <Header id="github-header-large" as="h3">Github: <a id="portfolio-github-large" href="https://github.com/jpgeib/jpgeib-portfolio" target=":blank">https://github.com/jpgeib/jpgeib-portfolio</a></Header>
-                                        <Header id="heroku-header-large" as="h3">Heroku: <a id="portfolio-heroku-large" href="https://jpgeib-portfolio.herokuapp.com/" target=":blank">https://jpgeib-portfolio.herokuapp.com/</a></Header>
-                                    </Responsive>
-                                </Responsive>
-                                <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Row}>
-                                    <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Column} width={16}>
-                                        <List bulleted size="big">
-                                            <List.Item className="project-list-item-large">An online portfolio created to list and show off my achievements/works in full-stack web development and digital media production.</List.Item>
-                                            <List.Item className="project-list-item-large">Role/Responsibility: Front-End Development, Front-End Design, Back-End Development</List.Item>
-                                            <List.Item className="project-list-item-large">Technologies used: React/Redux, CSS, Semantic UI React Framework, Express.js, Adobe Photoshop</List.Item>
-                                        </List>
-                                    </Responsive>
-                                </Responsive>
-                            </Responsive>
-                        </Responsive>
-                    </Responsive>
+                    <OnlinePortfolioLarge />
                     <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Row}>
                         <Responsive {...Responsive.onlyLargeScreen} minWidth={1572} maxWidth={1919} as={Grid.Column} width={8}>
                             <Image src={AllChatV2} size="big" />
