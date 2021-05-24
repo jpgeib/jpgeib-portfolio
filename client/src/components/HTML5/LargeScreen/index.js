c2b8336bcb09177657cd62b939831bbea06d655c
James Geib <jpgeib2@gmail.com>
2021-05-24 12:04:40 -0400

    set up screen view subcomponents for HTML5 skill entry

---
+++ client/src/components/HTML5/LargeScreen/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Grid, Header, Image, Responsive } from "semantic-ui-react";
+import HTML5 from "../../../assets/images/HTML-icon.png";
+
+class LargeScreen extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+};
+
+export default LargeScreen;
--- client/src/components/HTML5/WideScreen/index.js
+++ client/src/components/HTML5/WideScreen/index.js
@@ -2,3 +2,14 @@ import React, { Component } from "react";
 import { Grid, Header, Image, Responsive } from "semantic-ui-react";
 import HTML5 from "../../../assets/images/HTML-icon.png";
 
+class WideScreen extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+};
+
+export default WideScreen;
