d454db73704489e8239eeebcafbf4feb35736846
James Geib <jpgeib2@gmail.com>
2021-05-24 12:24:26 -0400

    set up subcomponent files for CSS entry in SkillSection

---
+++ client/src/components/CSS/Mobile/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Grid, Header, Image, Responsive } from "semantic-ui-react";
+import CSS from "../../../assets/images/CSS-icon.png";
+
+class Mobile extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+};
+
+export default Mobile;
