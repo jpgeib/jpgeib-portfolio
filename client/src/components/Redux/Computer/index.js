131070d44fa5c93165f2c654ce4777f470cf1046
James Geib <jpgeib2@gmail.com>
2021-05-25 12:02:38 -0400

    prepared computer view of remaining technical skill components for moving code

---
+++ client/src/components/Redux/Computer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Grid, Header, Image, Responsive } from "semantic-ui-react";
+
+class Computer extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+};
+
+export default Computer;
