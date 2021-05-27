64f936ef8b270e43fd512b374408578112c2ce34
James Geib <jpgeib2@gmail.com>
2021-05-27 15:20:28 -0400

    added new responsive logic in main SkillSection index.js and applied appropriate conditionals for each component instance

--- client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/SkillSection/index.js
@@ -6,12 +6,32 @@ import SkillSectionComputer from "./SkillSectionComputer";
 import "./style.css";
 
 class SkillSection extends Component {
+
+    state = {
+        windowWidth: window.innerWidth
+    }
+
+    handleResize = (e) => {
+        this.setState({ windowWidth: window.innerWidth });
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
     render() {
+
+        const { windowWidth } = this.state;
+
         return (
             <>
-                <SkillSectionMobile />
-                <SkillSectionTablet />
-                <SkillSectionComputer />            
+                {windowWidth <= 414 && <SkillSectionMobile />}
+                {(windowWidth >= 415 && windowWidth <= 799) && <SkillSectionTablet />}
+                {windowWidth >= 800 && <SkillSectionComputer />}            
             </>
         );
     }
