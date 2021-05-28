64d3b8d8e23cc77ff83f567af9ce59cf95ca6476
James Geib <jpgeib2@gmail.com>
2021-05-28 13:19:50 -0400

    deleted large and wide screen views for ProjectSection due to redundancy

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import ProjectSectionMobile from "./ProjectSectionMobile";
 import ProjectSectionTablet from "./ProjectSectionTablet";
 import ProjectSectionComputer from "./ProjectSectionComputer";
-import ProjectSectionLarge from "./ProjectSectionLarge";
-import ProjectSectionWide from "./ProjectSectionWide";
 
 class ProjectSection extends Component {
     render() {
@@ -12,8 +10,6 @@ class ProjectSection extends Component {
                 <ProjectSectionMobile />
                 <ProjectSectionTablet />
                 <ProjectSectionComputer />
-                <ProjectSectionLarge />
-                <ProjectSectionWide />
             </>
         );
     }
