fff039013694485d87ec1b16920d82fd3be14ee2
James Geib <jpgeib2@gmail.com>
2021-05-28 13:29:19 -0400

    added new responsive logic to main ProjectSection index.js

--- client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
+++ client/src/pages/Portfolio/WebDevPortfolio/ProjectSection/index.js
@@ -4,12 +4,32 @@ import ProjectSectionTablet from "./ProjectSectionTablet";
 import ProjectSectionComputer from "./ProjectSectionComputer";
 
 class ProjectSection extends Component {
+
+    state = {
+        windowWidth: window.innerWidth
+    }
+
+    handleResize = (e) => {
+        this.setState({ windowWidth: window.innerWidth });
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
     render() {
+
+        const  { windowWidth } = this.state;
+
         return (
             <>
-                <ProjectSectionMobile />
-                <ProjectSectionTablet />
-                <ProjectSectionComputer />
+                {windowWidth <= 414 && <ProjectSectionMobile />}
+                {(windowWidth >= 415 && windowWidth <= 799) && <ProjectSectionTablet />}
+                {windowWidth >= 800 && <ProjectSectionComputer />}
             </>
         );
     }
