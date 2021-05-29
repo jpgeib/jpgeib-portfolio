34d63973238adf4869c67eb8396850e0393e5a97
James Geib <jpgeib2@gmail.com>
2021-05-28 20:30:16 -0400

    applied the same changes to the tablet view of ProjectSection entries

--- client/src/components/AllChat/Tablet/index.js
+++ client/src/components/AllChat/Tablet/index.js
@@ -1,38 +1,38 @@
 import React, { Component } from "react";
-import { Grid, Header, Image, List, Responsive } from "semantic-ui-react";
+import { Grid, Header, Image, List } from "semantic-ui-react";
 import AllChatV1 from "../../../assets/screenshots/AllChatV1.png";
 
 class Tablet extends Component {
     render() {
         return (
             <>
-                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
-                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                <Grid.Row>
+                    <Grid.Column width={16}>
                         <Image id="allchatv1-image-tablet" src={AllChatV1} size="big" />
-                    </Responsive>
-                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
-                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="allchatv1-container-tablet">
-                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
-                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                    </Grid.Column>
+                    <Grid.Column width={16}>
+                        <Grid id="allchatv1-container-tablet">
+                            <Grid.Row>
+                                <Grid.Column width={16}>
                                     <Header id="allchatv1-header-tablet" as="h2">AllChat V1</Header>
-                                </Responsive>
-                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                                </Grid.Column>
+                                <Grid.Column width={16}>
                                     <Header id="github-header-tablet" as="h3">Github: <a id="allchatv1-github-tablet" href="https://github.com/paulmhan/AllChat" target=":blank">https://github.com/paulmhan/AllChat</a></Header>
                                     <Header id="heroku-header-tablet" as="h3">Heroku: <a id="allchatv1-heroku-tablet" href="https://all-chat-app.herokuapp.com/" target=":blank">https://all-chat-app.herokuapp.com/</a></Header>
-                                </Responsive>
-                            </Responsive>
-                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
-                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={16}>
                                     <List bulleted size="big">
                                         <List.Item className="project-list-item-tablet">A basic online chatroom app that serves as the foundational stepping stone for an even more ambitious project.</List.Item>
                                         <List.Item className="project-list-item-tablet">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
                                         <List.Item className="project-list-item-tablet">Technologies used: React, Semantic UI React Framework, CSS</List.Item>
                                     </List>
-                                </Responsive>
-                            </Responsive>
-                        </Responsive>
-                    </Responsive>
-                </Responsive>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Grid.Column>
+                </Grid.Row>
             </>
         );
     }
--- client/src/components/AllChatV2/Tablet/index.js
+++ client/src/components/AllChatV2/Tablet/index.js
@@ -1,38 +1,38 @@
 import React, { Component } from "react";
-import { Grid, Header, Image, List, Responsive } from "semantic-ui-react";
+import { Grid, Header, Image, List } from "semantic-ui-react";
 import AllChatV2 from "../../../assets/screenshots/AllChatV2.png";
 
 class Tablet extends Component {
     render() {
         return (
             <>
-                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
-                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                <Grid.Row>
+                    <Grid.Column width={16}>
                         <Image id="allchatv2-image-tablet" src={AllChatV2} size="big" />
-                    </Responsive>
-                    <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
-                        <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid} id="allchatv2-container-tablet">
-                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
-                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                    </Grid.Column>
+                    <Grid.Column width={16}>
+                        <Grid id="allchatv2-container-tablet">
+                            <Grid.Row>
+                                <Grid.Column width={16}>
                                     <Header id="allchatv2-header-tablet" as="h2">AllChat V2</Header>
-                                </Responsive>
-                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                                </Grid.Column>
+                                <Grid.Column width={16}>
                                     <Header id="github-header-tablet" as="h3">Github: <a id="allchatv2-github-tablet" href="https://github.com/paulmhan/AllChat-v2" target=":blank">https://github.com/paulmhan/AllChat-v2</a></Header>
                                     <Header id="heroku-header-tablet" as="h3">Heroku: <a id="allchatv2-heroku-tablet" href="https://all-chat-v2.herokuapp.com" target=":blank">https://all-chat-v2.herokuapp.com</a></Header>
-                                </Responsive>
-                            </Responsive>
-                            <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Row}>
-                                <Responsive {...Responsive.onlyTablet} minWidth={415} maxWidth={799} as={Grid.Column} width={16}>
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={16}>
                                     <List bulleted size="big">
                                         <List.Item className="project-list-item-tablet">An online chatroom app capable of translating messages from one language to another, broadening the scope of communication.</List.Item>
                                         <List.Item className="project-list-item-tablet">Role/Responsibility: Front-End Development, Front-End Design</List.Item>
                                         <List.Item className="project-list-item-tablet">Technologies used: React/Redux, Semantic UI React Framework, CSS, Adobe Photoshop</List.Item>
                                     </List>
-                                </Responsive>
-                            </Responsive>
-                        </Responsive>
-                    </Responsive>
-                </Responsive>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Grid.Column>
+                </Grid.Row>
             </>
         );
     }
