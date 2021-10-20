bce64352fa60d1b79a397c0c420584f3cdd1cbdd
James Geib <jpgeib2@gmail.com>
2021-10-20 19:37:47 -0400

    added symbols of main technologies to Footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Image, Grid, Header } from "semantic-ui-react";
+import ReactLogoWhite from "../../assets/images/React-Logo-White.png";
+import SemanticLogoWhite from "../../assets/images/semantic-ui-logo.png"
 
 import "./style.css";
 
 const Footer = (props) => {
     return (
-        <div id="footer">
-            <Header as="h1">This is the footer.</Header>
-        </div>
+        <Grid id="footer">
+            <Grid.Row>
+                <Grid.Column>
+                    <Header id="footer-header" as="h1">Powered by:</Header>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column width={1}>
+                    <Image id="footer-react-image" size="tiny" src={ReactLogoWhite} />
+                </Grid.Column>
+                <Grid.Column width={1}>
+                    <Image id="footer-semantic-image" size="tiny" src={SemanticLogoWhite} />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     );
 };
 
