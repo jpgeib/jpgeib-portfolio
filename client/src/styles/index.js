c2f85abe98de3f257b817e9162d186cf271b8d16
James Geib <jpgeib2@gmail.com>
2021-10-21 14:02:40 -0400

    translated vanilla css of tablet view of ProjectSection to an object variable

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -99,10 +99,65 @@ const projectStyle = {
         }
     },
     tablet: {
-
+        general: {
+            page: {
+                marginTop: "3%"
+            },
+            headerContainer: {
+                margin: "0 auto",
+                width: "70%"
+            },
+            header: {
+                fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+            },
+            subheader: {
+                fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                marginBottom: "1%"
+            },
+            container: {
+                backgroundColor: "rgb(180, 74, 70)",
+                margin: "0 auto",
+                width: "70%",
+                padding: "1%",
+                overflowY: "auto",
+                height: "715px"
+            }
+        },
+        subSection: {
+            image: {
+                marginTop: "2.5%"
+            },
+            linkHeader: {
+                color: "white",
+                fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+            },
+            listItem: {
+                color: "white",
+                fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+            },
+            header: {
+                color: "white",
+                fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                marginBotton: "5%"
+            },
+            link: {
+                color: "white",
+                fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                textDecorationLine: "underline"
+            },
+            container: {
+                border: "solid white 2px",
+                marginTop: "5%"
+            }
+        }
     },
     mobile: {
+        general: {
 
+        },
+        subSection: {
+
+        }
     }
 };
 
