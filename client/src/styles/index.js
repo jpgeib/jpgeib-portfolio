088f66f51983140a5133d6d430e4bf24e51bf3a4
James Geib <jpgeib2@gmail.com>
2021-10-25 15:46:35 -0400

    translated mobile view css of SkillSection to style object variable

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -515,92 +515,116 @@ const skillStyle = {
         entries: {
             html5: {
                 icon: {
-
+                    margin: "0 auto"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "2%",
+                    marginLeft: "33.5%"
                 }
             },
             css3: {
                 icon: {
-
+                    margin: "0 auto"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "2.5%",
+                    marginLeft: "42%"
                 }
             },
             javascript: {
                 icon: {
-
+                    margin: "0 auto"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "2%",
+                    marginLeft: "28%"
                 }
             },
             jQuery: {
                 icon: {
-
+                    margin: "0 auto"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "2%",
+                    marginLeft: "36%",
+                    marginBottom: "4%"
                 }
             },
             sql: {
                 icon: {
-
+                    margin: "0 auto"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "4%",
+                    marginLeft: "42%"
                 }
             },
             mongodb: {
                 icon: {
-
+                    marginLeft: "20%"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "1.5%",
+                    marginLeft: "30%"
                 }
             },
             react: {
                 icon: {
-
+                    margin: "0 auto"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "2%",
+                    marginLeft: "33%"
                 }
             },
             redux: {
                 icon: {
-
+                    marginTop: "11.75%",
+                    marginLefT: "20%"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "2%",
+                    marginLeft: "32%"
                 }
             },
             express: {
                 icon: {
-
+                    marginTop: "17%",
+                    marginLeft: "20%"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "15%",
+                    marginLeft: "29%"
                 }
             },
             canto: {
                 icon: {
-
+                    marginTop: "15%",
+                    marginLeft: "22%"
                 },
                 header: {
                     color: "white",
                     fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
+                    marginTop: "7%",
+                    marginLeft: "40%"
                 }
             }
         }
