3ca0306542fa70a556f081af16c053dff58b49a1
James Geib <jpgeib2@gmail.com>
2021-11-22 12:13:45 -0500

    divided Blog page into different screen view components

---
+++ client/src/pages/Blog/BlogComputer/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Grid, Image, Header } from "semantic-ui-react";
+import Placeholder from "../../../assets/screenshots/jpgeib-portfolio.png";
+
+class BlogComputer extends Component {
+    render() {
+
+        const { page, headerContainer, header, entry, container } = this.props.computer;
+
+        return (
+            <div style={page}>
+                <Grid style={headerContainer}>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Header style={header} as="h1">Web Developer Blog</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Header style={header} as="h3">This is where I post updates on what I'm currently working on, what I'm learning, or interesting discoveries I've found.</Header>
+                        </Grid.Column>
+                    </Grid.Row>   
+                </Grid>
+                <Grid style={container}>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="big" src={Placeholder} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Header style={entry} as="h2">10/21/21 - 12:08PM</Header>
+                            <Header style={entry} as="h3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad possimus sequi maxime placeat, suscipit delectus maiores nemo, optio minus ullam quae sunt. Adipisci sapiente possimus porro explicabo neque voluptatum placeat?</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Image size="big" src={Placeholder} />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Header style={entry} as="h2">10/21/21 - 12:08PM</Header>
+                            <Header style={entry} as="h3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad possimus sequi maxime placeat, suscipit delectus maiores nemo, optio minus ullam quae sunt. Adipisci sapiente possimus porro explicabo neque voluptatum placeat?</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        );
+    }
+};
+
+export default BlogComputer;
--- client/src/styles/blog/index.js
+++ client/src/styles/blog/index.js
@@ -25,9 +25,53 @@ export const blogStyle = {
         }
     },
     tablet: {
-
+        page: {
+            marginTop: "3%"
+        },
+        headerContainer: {
+            margin: "0 auto",
+            width: "70%",
+            marginBottom: "2%"
+        },
+        header: {
+            fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+        },
+        entry: {
+            color: "white",
+            fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+        },
+        container: {
+            backgroundColor: "rgb(180, 74, 70)",
+            margin: "0 auto",
+            width: "70%",
+            padding: "1%",
+            overflowY: "auto",
+            maxHeight: "715px"
+        }
     },
     mobile: {
-
+        page: {
+            marginTop: "3%"
+        },
+        headerContainer: {
+            margin: "0 auto",
+            width: "70%",
+            marginBottom: "2%"
+        },
+        header: {
+            fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+        },
+        entry: {
+            color: "white",
+            fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+        },
+        container: {
+            backgroundColor: "rgb(180, 74, 70)",
+            margin: "0 auto",
+            width: "70%",
+            padding: "1%",
+            overflowY: "auto",
+            maxHeight: "715px"
+        }
     }
 };
