5d61cac826ab185fe58f3b544c423a2b12ede225
James Geib <jpgeib2@gmail.com>
2021-11-22 12:14:17 -0500

    added responsive logic to Blog page

--- client/src/pages/Blog/index.js
+++ client/src/pages/Blog/index.js
@@ -6,13 +6,14 @@ import BlogTablet from "./BlogTablet";
 class Blog extends Component {
     render() {
 
+        const { width } = this.props;
         const { computer, tablet, mobile } = this.props.blog;
 
         return (
             <>
-                <BlogMobile mobile={mobile} />
-                <BlogTablet tablet={tablet} />
-                <BlogComputer computer={computer} />
+                {width <= 426 && <BlogMobile mobile={mobile} />}
+                {(width >= 427 && width <= 1023) && <BlogTablet tablet={tablet} />}
+                {width >= 1024 && <BlogComputer computer={computer} />}
             </>
         );
     }
