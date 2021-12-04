c8e07fc26c7e73ae1c629870a9355ef1297381dd
James Geib <jpgeib2@gmail.com>
2021-12-03 19:41:39 -0500

    made initial changes to first two blog entries on Blog page

--- client/src/pages/Blog/BlogComputer/index.js
+++ client/src/pages/Blog/BlogComputer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
-import Placeholder from "../../../assets/screenshots/jpgeib-portfolio.png";
+import StyleFolders from "../../../assets/screenshots/style-folder-structure.png";
+import FixingResponsive from "../../../assets/screenshots/fixing-responsive-logic.png";
 
 class BlogComputer extends Component {
     render() {
@@ -24,19 +25,21 @@ class BlogComputer extends Component {
                 <Grid style={container}>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Image size="big" src={Placeholder} />
+                            <Image size="big" src={FixingResponsive} />
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Header style={entry} as="h2">10/21/21 - 12:08PM</Header>
+                            <Header style={entry} as="h2">11/20/21 - 5:10 PM</Header>
                             <Header style={entry} as="h3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad possimus sequi maxime placeat, suscipit delectus maiores nemo, optio minus ullam quae sunt. Adipisci sapiente possimus porro explicabo neque voluptatum placeat?</Header>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Image size="big" src={Placeholder} />
+                        <Grid.Column width={1}></Grid.Column>
+                        <Grid.Column width={6}>
+                            <Image size="medium" src={StyleFolders} />
                         </Grid.Column>
+                        <Grid.Column width={1}></Grid.Column>
                         <Grid.Column width={8}>
-                            <Header style={entry} as="h2">10/21/21 - 12:08PM</Header>
+                            <Header style={entry} as="h2">11/16/21 - 6:53 PM</Header>
                             <Header style={entry} as="h3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad possimus sequi maxime placeat, suscipit delectus maiores nemo, optio minus ullam quae sunt. Adipisci sapiente possimus porro explicabo neque voluptatum placeat?</Header>
                         </Grid.Column>
                     </Grid.Row>
