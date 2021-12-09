aac33b4f562558bf4f7897482067e7fcc525548b
James Geib <jpgeib2@gmail.com>
2021-12-09 16:12:52 -0500

    constructed Computer view of Footer, converted vanilla CSS to inline styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,32 +1,17 @@
-import React from "react";
-import { Image, Grid, Header } from "semantic-ui-react";
-import ReactLogoWhite from "../../assets/images/React-Logo-White.png";
-import SemanticLogoWhite from "../../assets/images/semantic-ui-logo.png"
+import React, { Component } from "react";
+import FooterComputer from "./Computer";
 
-import "./style.css";
+class Footer extends Component {
+    render() {
 
-const Footer = (props) => {
-    return (
-        <Grid id="footer">
-            <Grid.Row>
-                <Grid.Column>
-                    <Header id="footer-header" as="h1">Powered by:</Header>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column width={1}>
-                    <a href="https://reactjs.org/" target=":blank">
-                        <Image id="footer-react-image" size="tiny" src={ReactLogoWhite} />
-                    </a>
-                </Grid.Column>
-                <Grid.Column width={1}>
-                    <a href="https://react.semantic-ui.com/" target=":blank">
-                        <Image id="footer-semantic-image" size="tiny" src={SemanticLogoWhite} />
-                    </a>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-    );
-};
+        const { computer } = this.props.footer;
+
+        return (
+            <>
+                <FooterComputer computer={computer} />
+            </>
+        );
+    }
+}
 
 export default Footer;
--- client/src/styles/footer/index.js
+++ client/src/styles/footer/index.js
@@ -1,3 +1,25 @@
 export const footerStyle = {
+    computer: {
+        footer: {
+            marginTop: "5%",
+            backgroundColor: "rgb(180, 74, 70)"
+        },
+        header: {
+            color: "white",
+            marginLeft: "1%",
+            fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+        },
+        react: {
+            marginLeft: "15%"
+        },
+        semantic: {
+            marginLeft: "15%"
+        }
+    },
+    tablet: {
 
+    },
+    mobile: {
+
+    }
 };
