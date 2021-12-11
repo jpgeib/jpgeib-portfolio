6c23e7fda5a557f032332b8a7dd2b23188fb095a
James Geib <jpgeib2@gmail.com>
2021-12-11 13:50:45 -0500

    adjusted column width of react and semantic icons in Footer component

--- client/src/styles/footer/index.js
+++ client/src/styles/footer/index.js
@@ -1,7 +1,7 @@
 export const footerStyle = {
     computer: {
         footer: {
-            marginTop: "5%",
+            marginTop: "6%",
             backgroundColor: "rgb(180, 74, 70)"
         },
         header: {
@@ -17,9 +17,37 @@ export const footerStyle = {
         }
     },
     tablet: {
-
+        footer: {
+            marginTop: "8%",
+            backgroundColor: "rgb(180, 74, 70)"
+        },
+        header: {
+            color: "white",
+            marginLeft: "1%",
+            fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+        },
+        react: {
+            marginLeft: "15%"
+        },
+        semantic: {
+            marginLeft: "15%"
+        }
     },
     mobile: {
-
+        footer: {
+            marginTop: "10%",
+            backgroundColor: "rgb(180, 74, 70)"
+        },
+        header: {
+            color: "white",
+            marginLeft: "1%",
+            fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+        },
+        react: {
+            marginLeft: "15%"
+        },
+        semantic: {
+            marginLeft: "15%"
+        }
     }
 };
