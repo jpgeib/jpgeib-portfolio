d8239e64f43d414c586f434d8b67ba9b9eee4f95
James Geib <jpgeib2@gmail.com>
2022-03-09 12:47:41 -0500

    troubleshooting reload error for heroku version of site

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"), function (err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    });
+});
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
