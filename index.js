169cd95385132f9c875fef74fac146869805f2e2
James Geib <jpgeib2@gmail.com>
2022-03-09 14:04:23 -0500

    minor adjustment to server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express();
 const server = require("http").createServer(app);
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
