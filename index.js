a300f0401116113dae226206339121b6a24664a1
James Geib <jpgeib2@gmail.com>
2022-03-09 13:22:36 -0500

    attempting a different approach

--- index.js
+++ index.js
@@ -12,14 +12,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-// }
-
 if (process.env.NODE_ENV === "production") {
-    const publicPath = path.join(__dirname, "client/build");
-    app.use(express.static(publicPath));
-    app.use("*", express.static(publicPath));
+    app.use(express.static("client/build"));
 }
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
